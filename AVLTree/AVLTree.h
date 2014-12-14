dbe4c57e29dda3a7444978f3867d32d8b42795cb
RogerChern <chenyuntao08@gmail.com>
2014-12-14 18:35:36 +0800

    implemented remove( ) and fixed a fatal bug in doubleRotateWithLeft/Right( )

--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -27,6 +27,21 @@ private:
     static constexpr int ALLOWED_IMBALANCE = 1;
     
 private:
+    Node * search(int key) {
+        Node *x = _root;
+        while (x != nullptr) {
+            if (key > x -> key) {
+                x = x -> right;
+            }
+            else if (key < x -> key) {
+                x = x -> left;
+            }
+            else {
+                return x;
+            }
+        }
+        return x;
+    }
     void insert(Node *n) {
         if (_root == nullptr) {
             _root = n;
@@ -53,8 +68,63 @@ private:
         balance(n -> parent);
     }
     
+    void remove(Node *n) {
+        if (n -> left == nullptr) {
+            transplant(n, n -> right);
+            balance(n -> parent);
+        }
+        else if (n -> right == nullptr) {
+            transplant(n, n -> left);
+            balance(n -> parent);
+        }
+        else {
+            Node *suc = findMin(n -> right);
+            if (suc == n -> right) {
+                transplant(n, suc);
+                suc -> left = n -> left;
+                suc -> left -> parent = suc;
+                balance(suc);
+            }
+            else {
+                Node *x = suc -> parent;
+                transplant(suc, suc -> right);
+                transplant(n, suc);
+                suc -> left = n -> left;
+                suc -> left -> parent = suc;
+                suc -> right = n -> right;
+                suc -> right -> parent = suc;
+                balance(x);
+            }
+        }
+    }
+    
+    Node * findMin(Node *n) {
+        Node *x = n;
+        Node *px = n;
+        while (x != nullptr) {
+            px = x;
+            x = x -> left;
+        }
+        return px;
+    }
+    
+    void transplant(Node *x, Node *y) {
+        if (x -> parent == nullptr) {
+            _root = y;
+        }
+        else if (x == x -> parent -> left) {
+            x -> parent -> left = y;
+        }
+        else {
+            x -> parent -> right = y;
+        }
+        if (y != nullptr) {
+            y -> parent = x -> parent;
+        }
+    }
+    
     int height(Node *n) {
-        return n == nullptr ? -1 : n -> height;
+        return (n == nullptr) ? -1 : n -> height;
     }
     
     void balance(Node *n) {
@@ -117,11 +187,13 @@ private:
     }
     
     void doubleRotateWithLeft(Node *n) {
+        --n -> left -> height;
         rotateWithRight(n -> left);
         rotateWithLeft(n);
     }
     
     void doubleRotateWithRight(Node *n) {
+        --n -> right -> height;
         rotateWithLeft(n -> right);
         rotateWithRight(n);
     }
@@ -129,6 +201,13 @@ public:
     void insert(int k, int v) {
         Node *n = new Node(k, v);
         insert(n);
+    }
+    
+    void remove(int k) {
+        Node *x = search(k);
+        if (x != nullptr) {
+            remove(x);
+        }
     }
 };
 
