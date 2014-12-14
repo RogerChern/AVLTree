6960a06cf5b11c5feb627daf3db3d6dc23d7c7f4
RogerChern <chenyuntao08@gmail.com>
2014-12-14 18:36:10 +0800

    write a randomized insert / remove test for AVL Tree

--- AVLTree/main.cpp
+++ AVLTree/main.cpp
@@ -8,6 +8,9 @@
 
 #include <iostream>
 #include <ctime>
+#include <vector>
+#include <random>
+#include <map>
 #include "AVLTree.h"
 
 using namespace std;
@@ -15,12 +18,29 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     AVLTree avltree;
-    auto begin = clock();
+    map<int, int> mp;
+    vector<int> test1;
+    vector<int> test2;
     for (int i = 0; i < 1000000; ++i) {
+        test1.push_back(i);
+        test2.push_back(i);
+    }
+    shuffle(test1.begin(), test1.end(), mt19937_64(clock()));
+    shuffle(test2.begin(), test2.end(), mt19937_64(clock()));
+    
+    
+    auto begin = clock();
+    for (int i : test1) {
         avltree.insert(i, i);
     }
+    
+    for (int i : test2) {
+        avltree.remove(i);
+    }
+    
     auto end = clock();
     
-    cout << double(end - begin) / CLOCKS_PER_SEC << "ms" << endl;
+    
+    cout << double(end - begin) / CLOCKS_PER_SEC * 1000 << "ms" << endl;
     return 0;
 }
